1fe8b6e4c7d82b04f93f1404432e6e58134564b5
codebuster154 <kumardeepsan007@gmail.com>
2025-12-16 17:10:45 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")      # name input
+age = input("How old are you? ")         # age input
+hobby = input("What is your favorite hobby? ")  # hobby input
+
+print(f"\n Welcome {name}! ")        # welcome message
+print(f"You are {age} years old and love {hobby}.")  # details
